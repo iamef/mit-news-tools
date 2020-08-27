c55da059971f484bb54dea632acb078a484e5879
Emily Fan <emilyfan8@gmail.com>
2020-08-27 10:13:38 -0400

    Rename function to asciify and return_failed_chars option

--- mitnewstools/asciify.py
+++ mitnewstools/asciify.py
@@ -9,11 +9,11 @@ last edited: 7/1
 from confusables import normalize
 
 
-def faster_normalize(text: str):
+def asciify(text: str, return_failed_chars=False):
     retstr = ""
 
     numconvchar = 0
-    numfailedchar = 0
+    failedchars = []
 
     for char in text:
         if not char.isascii():
@@ -40,10 +40,10 @@ def faster_normalize(text: str):
 
                 newchar = " "
 
-                numfailedchar+=1
+                failedchars.append(char)
 
             else:
-                numconvchar+=1
+                numconvchar += 1
             # elif newchar not in ["'", '"', "...", '-']:
             # print("YAY: " + char + " -> "+ newchar)
             retstr += newchar
@@ -52,4 +52,6 @@ def faster_normalize(text: str):
 
     # print(str(numconvchar) + ' characters conversted to ASCII | ' + str(numfailedchar) + " failed")
 
+    if return_failed_chars:
+        return retstr, failedchars
     return retstr
